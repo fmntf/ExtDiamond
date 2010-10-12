105b1dcf759ebec3447788d6e0fecd8a10bc006c
Francesco Montefoschi <francesco.monte@gmail.com>
2010-10-12 12:22:26 +0200

    added Ext 3.3 example

---
+++ Example/public/actioncolumn-grid.js
@@ -0,0 +1,85 @@
+Ext.onReady(function()
+{
+
+	FirewallGrid = Ext.extend(Ext.grid.GridPanel, {
+
+		accept: function(record)
+		{
+			alert('Accept: ' + record.get('name'));
+		},
+
+		deny: function(record)
+		{
+			alert('Deny: ' + record.get('name'));
+		}
+
+	});
+
+	var grid = new FirewallGrid(
+	{
+		height: 200,
+		width: 500,
+		autoExpandColumn: 'name',
+		store: new Ext.data.ArrayStore({
+			fields: [
+			   {name: 'name'},
+			   {name: 'description'}
+			]
+			,
+			data: [
+				['wupdate.exe', 'Windows Update client'],
+				['svchost.dll', 'Service host'],
+				['firefox.exe', 'Firefox Browser'],
+				['soffice.bin', 'OpenOffice.org'],
+				['iexplore.exe', 'Internet Explorer Browser (?)'],
+				['a928dds.exe', 'I promise, I\'m not a virus!']
+			],
+			autoLoad: true
+		}),
+		columns: [
+			{
+				id: 'name',
+				header : 'Application',
+				dataIndex: 'name'
+			},
+			{
+				id: 'description',
+				header : 'Description',
+				dataIndex: 'description'
+			},
+			{
+				header: 'Action',
+				xtype: 'actioncolumn',
+				width: 50,
+				items: [
+					{
+						icon: 'resources/fam/cross.gif',
+						tooltip: 'Deny',
+						handler: function(firewall, rowIndex, colIndex) {
+							var record = firewall.getStore().getAt(rowIndex);
+							firewall.deny(record);
+						}
+					},
+					{
+						icon: 'resources/fam/accept.png',
+						tooltip: 'Accept',
+						handler: function(firewall, rowIndex, colIndex) {
+							var record = firewall.getStore().getAt(rowIndex);
+							firewall.accept(record);
+						}
+					}
+				]
+			}
+		]
+	});
+
+	var window = new Ext.Window({
+		title: 'My Firewall',
+		items: [
+			grid
+		]
+	});
+
+	window.show();
+
+});
