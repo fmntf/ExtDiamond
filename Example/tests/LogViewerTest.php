f91253ab469ecc09b9f0bd81e5cafad652c20c0a
Francesco Montefoschi <francesco.monte@gmail.com>
2010-10-15 01:42:34 +0200

    created new test

---
+++ Example/tests/LogViewerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * ExtDiamond - Easy testing for ExtJS
+ * Copyright (C) 2010 Francesco Montefoschi <francesco.monte@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.html  GNU GPL 3.0
+ */
+
+class LogViewerTest extends ExtDiamond_TestCase
+{
+
+	protected $url = 'http://extdiamond.local/logviewer.html';
+
+	public function testOnPageLoadBothDateAndLogsAreEmpty()
+	{
+		$form = $this->getComponentByClass('logviewer');
+
+		$this->assertEquals('', $form->datepicker->getValue());
+		$this->assertEquals('', $form->eventlist->getValue());
+	}
+
+	public function testSelectingADateWillLoadLogs()
+	{
+		$form = $this->getComponentByClass('logviewer');
+		$date = $form->datepicker->handleAs('DateField');
+
+		$date->pick('03-23-2010');
+		$logs = $form->eventlist->getValue();
+
+		$this->assertNotEquals('', $form->eventlist->getValue(),
+			'The logs were not loaded');
+		$this->assertTrue((bool)strstr($logs, '2010-03-23'),
+			'The logs seems to be not about the picked date');
+	}
+
+	public function testChangingADateWillReloadLogs()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testCanDestroyEverything()
+	{
+		$this->markTestIncomplete();
+	}
+
+}
